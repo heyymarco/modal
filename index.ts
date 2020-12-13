6d9213af33fe9d9fcb0b805bf8a1f595ef03d7d8
heyymarco <yunuzk@gmail.com>
2020-12-13 16:45:32 +0630

    add dependency: ElementConfig

--- index.ts
+++ index.ts
@@ -1,4 +1,6 @@
 import Element from "@heymarco/element";
+import ElementConfig from "@heymarco/element-config";
+
 
 
 /**
@@ -6,37 +8,12 @@ import Element from "@heymarco/element";
  */
 export default class Modal {
     /**
-     * A function to be executed when an element with attribute data-modal="selector" is clicked.
-     */
-    static clickHandler(event : MouseEvent) {
-        // get the requested selector to be shown
-        // from the attribute data-modal="#the-requested-element-id" or data-modal=".the-requested-class"
-        const selector = (new Element(event.target as HTMLElement)).data(this.varPrefix) as string;
-
-        // set active of the first matching element:
-        this.setActive((new Element(selector)).first());
-    }
-
-    /**
-     * A function to be executed when the overlay (a blurry background) of the modal is clicked.
-     */
-    static overlayClickHandler(event : MouseEvent) {
-        const overlay = new Element(event.target as HTMLElement);
-
-        // make sure the clicked element is the overlay itself, not the content inside overlay.
-        // if the element has class .modal => it's the overlay element, otherwise the content
-        if (overlay.is(this.class)) this.setActive(null); // set active element to nothing (close the previously actived one).
-    }
-
-
-
-    /**
      * a variable to remember the previously shown modal.
      */
     static _activeModal : Element | null = null;
 
     /**
-     * 
+     * Set the active modal.
      * @param selector an element to be shown (can be HTMLElement or selector string or jQuery object).
      * Set to null to hide the previous shown element.
      */
@@ -51,7 +28,7 @@ export default class Modal {
 
             this._activeModal = element; // remember the shown element, so we can hide it later
 
-            Element.body.addClass(`${this.varPrefix}-open`); // disable the browser's scrollbar when modal is open
+            Element.body.addClass(`${Modal.config.varPrefix}-open`); // disable the browser's scrollbar when modal is open
         } else {
             // hide the previously shown element (if any):
             if (this._activeModal) {
@@ -67,66 +44,78 @@ export default class Modal {
             // make sure the scrollbar enabled when the modal's hiding animation was completed.
             // the easiest way (lazy man) is assuming the animation no longer than 1000ms, so it's safe to re-enable it.
             setTimeout(() => {
-                Element.body.removeClass(`${this.varPrefix}-open`);
+                Element.body.removeClass(`${Modal.config.varPrefix}-open`);
             }, 1000);
         }
     }
 
     /**
      * A shortcut to hide the previously shown modal.
-     * Equivalent to setActive(null)
+     * Equivalents to setActive(null)
      */
     static setInactive() { this.setActive(null); }
 
 
 
-    static _class = "";
-    static _varPrefix = "";
     /**
-     * @returns default returning ".modal"
+     * A function to be executed when the modal's trigger (ex: button) with attribute data-modal="selector" was clicked.
      */
-    static get class() : string {
-        return this._class;
+    static triggerClickHandler(event : MouseEvent) {
+        // get the requested selector to be executed
+        // from the attribute data-modal="#the-requested-element-id" or data-modal=".the-requested-class"
+        const selector = (new Element(event.target as HTMLElement)).data(Modal.config.varPrefix) as string;
+
+        // set active of the first matching element:
+        this.setActive((new Element(selector)).first());
     }
 
-    static _clickHandler = (event: JQuery.ClickEvent<HTMLElement, undefined, any, any>) => Modal.clickHandler(event as unknown as MouseEvent);
-    static _overlayClickHandler = (event: JQuery.ClickEvent<HTMLElement, undefined, any, any>) => Modal.overlayClickHandler(event as unknown as MouseEvent);
     /**
-     * Customize the ".modal" class name.
+     * A function to be executed when the modal or any contents inside the modal was clicked.
      */
-    static set class(name : string) {
-        if (this._class == name) return; // if its already the same, nothing to do.
+    static clickHandler(event : MouseEvent) {
+        const modal = new Element(event.target as HTMLElement);
 
-        if (this._class != "") { // detach prev event (if has set)
+        // make sure the clicked element is the overlay itself, not the content inside overlay.
+        // if the element has class .modal => it's the overlay element, otherwise the content
+        if (modal.is(Modal.config.class)) this.overlayClickHandler(event);
+    }
+
+    /**
+     * A function to be executed when the overlay (a blurry background) of the modal was clicked.
+     */
+    static overlayClickHandler(event : MouseEvent) {
+        this.setActive(null); // set active modal to nothing (close the previously actived one).
+    }
+
+
+
+    static _triggerClickHandler = (event: JQuery.ClickEvent<HTMLElement, undefined, any, any>) => Modal.triggerClickHandler(event as unknown as MouseEvent);
+    static _clickHandler        = (event: JQuery.ClickEvent<HTMLElement, undefined, any, any>) => Modal.clickHandler(event as unknown as MouseEvent);
+    static config = new ElementConfig(
+        /* className    = */ ".modal",
+        /* varPrefix    = */ "modal",
+        /* deconfigure  = */ () => {
             Element.document
-            .off("click", `[data-${this.varPrefix}]`, this._clickHandler)
-            .off("click", this.class, this._overlayClickHandler)
+
+            // un-watch the click event of modal's trigger with attribute data-modal="something" in whole document.
+            .off("click", `[data-${Modal.config.varPrefix}]`, Modal._triggerClickHandler)
+
+            // un-watch the click event of modal's overlay in whole document.
+            .off("click", Modal.config.class, Modal._clickHandler)
             ;
-        }
-
-
-        this._class = name;
-        this._varPrefix = (name || "").replace(".", "");
-        if (this._class != "") {
+        },
+        /* configure    = */ () => {
             Element.document
 
-            // watch the click event of all element with attribute data-modal="something" in whole document.
-            // when the element (ex: button) is clicked, the modal should be shown (if data-modal="something") or hidden (if data-modal="").
-            .on("click", `[data-${this.varPrefix}]`, this._clickHandler)
+            // watch the click event of modal's trigger with attribute data-modal="something" in whole document.
+            // when the modal's trigger (ex: button) is clicked, the modal should be shown (if the data-modal="something") or hidden (if data-modal="").
+            .on("click", `[data-${Modal.config.varPrefix}]`, Modal._triggerClickHandler)
 
-            // watch the click event of all element with class .modal in whole document.
-            // when the modal's overlay clicked, the modal sholud be hidden.
-            .on("click", this.class, this._overlayClickHandler)
+            // watch the click event of modal's overlay in whole document.
+            // when the modal's overlay is clicked, the modal should be hidden.
+            .on("click", Modal.config.class, Modal._clickHandler)
             ;
-        } // if
-    }
-
-    /**
-     * @returns default returning "modal"
-     */
-    static get varPrefix() : string {
-        return this._varPrefix;
-    }
+        },
+        /* configFirst  = */ true // it's safe to apply the config immediately.
+    );
 }
-
-Modal.class = ".modal";
